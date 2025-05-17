befb3e11bb09344620bc8965262322452ff3650c
Jonatan <jonatanav255@hotmail.com>
2025-05-16 20:43:12 -0600

    getting everything to start

---
+++ src/main/java/com/example/TinyURL/TinyUrlApplication.java
@@ -0,0 +1,14 @@
+package com.example.TinyURL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyUrlApplication.class, args);
+        System.out.println("HIIIII");
+    }
+
+}
